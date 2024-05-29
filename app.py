a6daa0178c6b3575c26cecf57c9dc990a5dbe198
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2024-05-28 23:02:53 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, render_template, request
 from diagnostic_bot import Diagnostic_bot
+import os
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 d_bot = Diagnostic_bot()
@@ -26,4 +28,4 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
